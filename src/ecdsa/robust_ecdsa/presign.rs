b64c186a9fd699aa02553d097d9825fe6e28a962
Simon Rastikian <157731593+srastikian@users.noreply.github.com>
2025-06-09 17:26:52 +0200

    Serialization problem solved and can interpolate a the polynomial fw(0) to share w

--- src/ecdsa/robust_ecdsa/presign.rs
+++ src/ecdsa/robust_ecdsa/presign.rs
@@ -2,17 +2,20 @@ use rand_core::OsRng;
 use frost_secp256k1::{
     Secp256K1Sha256, Secp256K1Group, Secp256K1ScalarField,
     Group, Field,
-    keys::SigningShare,
+    keys::{
+        SigningShare,
+        VerifyingShare
+    },
 };
-
 
 use crate::{
     crypto::polynomials::{
         evaluate_multi_polynomials,
-        generate_secret_polynomial
+        generate_secret_polynomial,
+        eval_interpolation,
     },
     ecdsa::KeygenOutput,
-    participants::{ParticipantCounter, ParticipantList},
+    participants::{ParticipantCounter, ParticipantList, ParticipantMap},
     protocol::{internal::SharedChannel, Participant, ProtocolError},
 };
 
@@ -107,9 +110,9 @@ async fn do_presign(
     // Evaluate my secret shares for my polynomials
     let shares = evaluate_five_polynomials([&my_fk, &my_fa, &my_fb, &my_fd, &my_fe], me)?;
     // Extract the shares into a vec of scalars
-    let mut shares = shares.iter()
+    let mut shares: Vec<Scalar> = shares.iter()
         .map( |signing_share| signing_share.to_scalar())
-        .collect::<Vec<_>>();
+        .collect();
 
     // Round 1
     // Receive evaluations from all participants
@@ -129,29 +132,39 @@ async fn do_presign(
 
     // Compute R_me = g^{k_me}
     let big_r_me = Secp256K1Group::generator() * shares[0];
+    let big_r_me = VerifyingShare::new(big_r_me);
+
     // Compute w_me = a_me * k_me + b_me
     let w_me = shares[1] * shares[0] + shares[2];
     let w_me = SigningShare::new(w_me);
 
     // Send and receive
     let wait_round_1 = chan.next_waitpoint();
-    chan.send_many(wait_round_1, &(&big_r_me.to_affine(), &w_me));
+    chan.send_many(wait_round_1, &(&big_r_me, &w_me));
+
+    // Store the sent items
+    let mut signingshares_map = ParticipantMap::new(&participants);
+    let mut verifyingshares_map = ParticipantMap::new(&participants);
+    signingshares_map.put(me, w_me);
+    verifyingshares_map.put(me, big_r_me);
 
     // Receive and interpolate
     seen.clear();
     seen.put(me);
     while !seen.full() {
-        let (from, (big_r_p, w_p)): (_ , (AffinePoint, SigningShare)) = chan.recv(wait_round_1).await?;
+        let (from, (big_r_p, w_p)): (_ , (VerifyingShare, SigningShare)) = chan.recv(wait_round_1).await?;
         if !seen.put(from) {
             continue;
         }
-        // collect big_r_p in a map
-        // collect w_p in a map
-
+        // collect big_r_p and w_p in maps that will be later ordered
+        signingshares_map.put(from, w_p);
+        verifyingshares_map.put(from, big_r_p);
     }
-        // transform both maps into sorted vectors
-        // exponent interpolation of big R
-        // exponent interpolation of w
+
+    // polynomial interpolation of w
+    let w = eval_interpolation(&signingshares_map, None);
+    // exponent interpolation of big R
+    // CAREFUL NOT TO INTERPOLATE MYSELF?
 
     Ok(())
 }
