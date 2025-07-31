4ba712f0e87a9e7ca17437b145712fbf8ee1eeee
Simon Rastikian <157731593+srastikian@users.noreply.github.com>
2025-07-31 11:57:09 +0200

    Keygen struct generalization

--- src/lib.rs
+++ src/lib.rs
@@ -10,3 +10,16 @@ pub mod eddsa;
 
 #[cfg(test)]
 mod test;
+
+
+
+use crypto::ciphersuite::Ciphersuite;
+use frost_core::{keys::SigningShare, VerifyingKey};
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Deserialize, Serialize, Eq, PartialEq)]
+/// Generic type of key pairs
+pub struct KeygenOutput<C: Ciphersuite> {
+    pub private_share: SigningShare<C>,
+    pub public_key: VerifyingKey<C>,
+}
