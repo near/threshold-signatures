2cebf76591c2a9b7f78bb73c1e014ced731097e1
Simon Rastikian <157731593+srastikian@users.noreply.github.com>
2025-08-01 11:58:48 +0200

    refactor ot-based ecdsa (changing types and reorganizing file placements)

---
+++ src/ecdsa/ot_based_ecdsa/triples/test.rs
@@ -0,0 +1,50 @@
+use rand_core::CryptoRngCore;
+
+use super::{TriplePub, TripleShare};
+#[cfg(test)]
+use crate::protocol::ProtocolError;
+use crate::{
+    ecdsa::{Field, Polynomial, ProjectivePoint, Secp256K1ScalarField},
+    protocol::Participant,
+};
+
+/// Create a new triple from scratch.
+///
+/// This can be used to generate a triple if you then trust the person running
+/// this code to forget about the values they generated.
+/// We prevent users from using it in non-testing env and attribute it to #[cfg(test)]
+#[cfg(test)]
+pub fn deal(
+    rng: &mut impl CryptoRngCore,
+    participants: &[Participant],
+    threshold: usize,
+) -> Result<(TriplePub, Vec<TripleShare>), ProtocolError> {
+    let a = Secp256K1ScalarField::random(&mut *rng);
+    let b = Secp256K1ScalarField::random(&mut *rng);
+    let c = a * b;
+
+    let f_a = Polynomial::generate_polynomial(Some(a), threshold - 1, rng)?;
+    let f_b = Polynomial::generate_polynomial(Some(b), threshold - 1, rng)?;
+    let f_c = Polynomial::generate_polynomial(Some(c), threshold - 1, rng)?;
+
+    let mut shares = Vec::with_capacity(participants.len());
+    let mut participants_owned = Vec::with_capacity(participants.len());
+
+    for p in participants {
+        participants_owned.push(*p);
+        shares.push(TripleShare {
+            a: f_a.eval_on_participant(*p).0,
+            b: f_b.eval_on_participant(*p).0,
+            c: f_c.eval_on_participant(*p).0,
+        });
+    }
+
+    let triple_pub = TriplePub {
+        big_a: (ProjectivePoint::GENERATOR * a).into(),
+        big_b: (ProjectivePoint::GENERATOR * b).into(),
+        big_c: (ProjectivePoint::GENERATOR * c).into(),
+        participants: participants_owned,
+        threshold,
+    };
+    Ok((triple_pub, shares))
+}
