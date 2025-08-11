0b75cbc35c5562af924c61a417e8543e8a14b8ea
SimonRastikian <>
2025-08-11 11:04:01 +0200

    clippy

--- src/ecdsa/ot_based_ecdsa/triples/generation.rs
+++ src/ecdsa/ot_based_ecdsa/triples/generation.rs
@@ -791,9 +791,9 @@ async fn do_generation_many<const N: usize>(
 
                 big_e_j_zero_v[i].put(from, their_big_e.eval_at_zero());
 
-                big_e_v[i] = big_e_v[i].add(&their_big_e)?;
-                big_f_v[i] = big_f_v[i].add(&their_big_f)?;
-                big_l_v[i] = big_l_v[i].add(&their_big_l)?;
+                big_e_v[i] = big_e_v[i].add(their_big_e)?;
+                big_f_v[i] = big_f_v[i].add(their_big_f)?;
+                big_l_v[i] = big_l_v[i].add(their_big_l)?;
             }
         }
 
