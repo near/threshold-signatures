0fff4056674a014a6e93a367efc3080e26f5a108
Simon Rastikian <43679791+SimonRastikian@users.noreply.github.com>
2025-09-25 15:10:57 +0200

    no constructor instead randomize_presign

--- src/ecdsa/robust_ecdsa/mod.rs
+++ src/ecdsa/robust_ecdsa/mod.rs
@@ -48,7 +48,7 @@ pub struct RerandomizedPresignOutput {
 }
 
 impl RerandomizedPresignOutput {
-    pub fn new(
+    pub fn rerandomize_presign(
         presignature: &PresignOutput,
         tweak: &Tweak,
         args: &RerandomizationArguments,
