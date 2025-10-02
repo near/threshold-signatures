8150ed7a7ce29bc937799526bed7acb9bda3adea
gilcu3 <gilcu3@gmail.com>
2025-10-02 09:03:02 +0200

    fix: misplaced comment

--- src/lib.rs
+++ src/lib.rs
@@ -16,12 +16,13 @@ pub use frost_core;
 pub use frost_ed25519;
 pub use frost_secp256k1;
 
-// For benchmark
+
 pub use crypto::ciphersuite::Ciphersuite;
+pub use participants::ParticipantList;
+// For benchmark
 pub use crypto::polynomials::{
     batch_compute_lagrange_coefficients, batch_invert, compute_lagrange_coefficient,
 };
-pub use participants::ParticipantList;
 
 use frost_core::serialization::SerializableScalar;
 use frost_core::{keys::SigningShare, Group, VerifyingKey};
