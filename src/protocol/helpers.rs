d880fabade1f489359ccc221abed0916239db9ab
Charly <github.womankind045@passinbox.com>
2025-10-02 19:19:31 +0330

    remove unused function

--- src/protocol/helpers.rs
+++ src/protocol/helpers.rs
@@ -1,23 +1,6 @@
 //! Helper functions for the protocol.
 use super::{internal::SharedChannel, Participant, ProtocolError};
 
-/// Receive a message from a specific participant.
-pub async fn recv_from<T>(
-    chan: &mut SharedChannel,
-    from: Participant,
-    wait: u64,
-) -> Result<T, ProtocolError>
-where
-    T: serde::de::DeserializeOwned,
-{
-    loop {
-        match chan.recv(wait).await? {
-            (p, msg) if p == from => return Ok(msg),
-            _ => {}
-        }
-    }
-}
-
 /// Gather exactly one message from each participant in a group before proceeding.
 ///
 /// Note: Result vector order depends on the order messages arrive.
