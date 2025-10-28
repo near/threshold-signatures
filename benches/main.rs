ba408482ad657762ca1f91d356667ec9f55761c8
Simon Rastikian <43679791+SimonRastikian@users.noreply.github.com>
2025-10-28 10:35:07 +0100

    No more unused functions

--- benches/main.rs
+++ benches/main.rs
@@ -3,6 +3,7 @@ mod protocol_benchmarks;
 
 use criterion::{criterion_group, criterion_main};
 use crypto_benchmarks::{inversion, lagrange};
+use protocol_benchmarks::naive_benchmarks::{ot_based_ecdsa, robust_ecdsa};
 
 criterion_group!(
     crypto_benchmarks,
@@ -11,4 +12,18 @@ criterion_group!(
     inversion::bench_inversion,
 );
 
-criterion_main!(crypto_benchmarks);
+criterion_group!(
+    ot_based_ecdsa_naive,
+    ot_based_ecdsa::bench_triples,
+    ot_based_ecdsa::bench_presign,
+    ot_based_ecdsa::bench_sign,
+);
+
+criterion_group!(
+    robust_ecdsa_naive,
+    robust_ecdsa::bench_presign,
+    robust_ecdsa::bench_sign,
+);
+
+// criterion_main!(crypto_benchmarks);
+criterion_main!(ot_based_ecdsa_naive, robust_ecdsa_naive);
