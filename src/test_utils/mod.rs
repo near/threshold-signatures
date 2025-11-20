56654e3ba021823417623f9e6b8ad99bb132eeeb
Simon Rastikian <43679791+SimonRastikian@users.noreply.github.com>
2025-11-20 14:53:05 +0100

    cargo fmt

--- src/test_utils/mod.rs
+++ src/test_utils/mod.rs
@@ -13,11 +13,11 @@ mod presign;
 mod run_protocol;
 mod sign;
 
-use rand_core::CryptoRngCore;
-use crate::participants::Participant;
 use crate::errors::ProtocolError;
+use crate::participants::Participant;
 use crate::protocol::Protocol;
 use crate::KeygenOutput;
+use rand_core::CryptoRngCore;
 
 /// Type representing DKG output keys
 pub type GenOutput<C> = Vec<(Participant, KeygenOutput<C>)>;
@@ -27,11 +27,11 @@ pub type GenProtocol<C> = Vec<(Participant, Box<dyn Protocol<Output = C>>)>;
 pub use mockrng::MockCryptoRng;
 
 pub use dkg::{assert_public_key_invariant, run_keygen, run_refresh, run_reshare};
+pub use mpc_interface::*;
 pub use participants::{generate_participants, generate_participants_with_random_ids};
 pub use presign::ecdsa_generate_rerandpresig_args;
 pub use run_protocol::{run_protocol, run_two_party_protocol};
 pub use sign::{check_one_coordinator_output, run_sign};
-pub use mpc_interface::*;
 
 /// Checks that the list contains all None but one element
 /// and verifies such element belongs to the coordinator
