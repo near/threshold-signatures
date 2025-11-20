344b0b1a2100360c10d0aca3d3d72e3e66cd11a3
Simon Rastikian <43679791+SimonRastikian@users.noreply.github.com>
2025-11-20 15:29:56 +0100

    renamping mpc_interface to test_generators

--- src/test_utils/mod.rs
+++ src/test_utils/mod.rs
@@ -7,7 +7,7 @@
 
 mod dkg;
 mod mockrng;
-pub mod mpc_interface;
+pub mod test_generators;
 mod participants;
 mod presign;
 mod run_protocol;
@@ -27,7 +27,7 @@ pub type GenProtocol<C> = Vec<(Participant, Box<dyn Protocol<Output = C>>)>;
 pub use mockrng::MockCryptoRng;
 
 pub use dkg::{assert_public_key_invariant, run_keygen, run_refresh, run_reshare};
-pub use mpc_interface::*;
+pub use test_generators::*;
 pub use participants::{generate_participants, generate_participants_with_random_ids};
 pub use presign::ecdsa_generate_rerandpresig_args;
 pub use run_protocol::{run_protocol, run_two_party_protocol};
