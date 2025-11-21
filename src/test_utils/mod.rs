da21da3e7c9fc6b90039333b9eb7cf05baaf6ae7
Simon Rastikian <43679791+SimonRastikian@users.noreply.github.com>
2025-11-21 18:03:57 +0100

    cargo fmt

--- src/test_utils/mod.rs
+++ src/test_utils/mod.rs
@@ -27,8 +27,8 @@ pub type GenProtocol<C> = Vec<(Participant, Box<dyn Protocol<Output = C>>)>;
 /// Type for a deterministic RNG
 pub use mockrng::MockCryptoRng;
 
-pub use mockrng::create_rngs;
 pub use dkg::{assert_public_key_invariant, run_keygen, run_refresh, run_reshare};
+pub use mockrng::create_rngs;
 pub use participants::{generate_participants, generate_participants_with_random_ids};
 pub use presign::ecdsa_generate_rerandpresig_args;
 pub use protocol::{run_protocol, run_protocol_and_take_snapshots, run_two_party_protocol};
