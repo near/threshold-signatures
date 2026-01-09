3313b1f6769c491f8d20cea7c755b6df49a42277
SimonRastikian <43679791+SimonRastikian@users.noreply.github.com>
2026-01-09 15:31:25 +0100

    Nothing too interesting, organizing some API

--- src/eddsa/redjubjub/mod.rs
+++ src/eddsa/redjubjub/mod.rs
@@ -6,21 +6,24 @@ mod test;
 use crate::crypto::ciphersuite::{BytesOrder, Ciphersuite, ScalarSerializationFormat};
 
 // JubJub Curve
-pub type JubjubBlake2b512 = reddsa::frost::redjubjub::JubjubBlake2b512;
-// JubJub Field
-pub type JubjubScalarField = reddsa::frost::redjubjub::JubjubScalarField;
-// JubJub Group
-pub type JubjubGroup = reddsa::frost::redjubjub::JubjubGroup;
-
-pub type KeygenOutput = crate::KeygenOutput<JubjubBlake2b512>;
-
+pub use reddsa::frost::redjubjub::JubjubBlake2b512;
 impl ScalarSerializationFormat for JubjubBlake2b512 {
     fn bytes_order() -> BytesOrder {
         BytesOrder::LittleEndian
     }
 }
-
 impl Ciphersuite for JubjubBlake2b512 {}
 
+
+pub type KeygenOutput = crate::KeygenOutput<JubjubBlake2b512>;
 /// Signature would be Some for coordinator and None for other participants
 pub type SignatureOption = Option<frost_core::Signature<JubjubBlake2b512>>;
+
+/// Abstracts over different RedJubJub parameter choices, [`Binding`]
+/// and [`SpendAuth`].
+pub use reddsa::SpendAuth;
+pub use reddsa::Binding;
+
+
+/// Randomizers for key pairs
+use reddsa::SigningKey;
