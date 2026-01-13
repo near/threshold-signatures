0fec75e1a64ca104e5cee80b0cafc3780a49b30b
SimonRastikian <43679791+SimonRastikian@users.noreply.github.com>
2026-01-13 17:02:52 +0100

    Signing From Frost

--- src/eddsa/redjubjub/mod.rs
+++ src/eddsa/redjubjub/mod.rs
@@ -1,7 +1,15 @@
-//! This module serves as a wrapper for Ed25519 scheme.
+//! This module serves as a wrapper for distributed RedDSA a.k.a. rerandomized FROST from [GK](https://eprint.iacr.org/2024/436.pdf)
+//! This is implemented on JubJub Curve with only the "Spend Authorization" set of parameters.
+//! Check https://zips.z.cash/protocol/protocol.pdf#concretespendauthsig for more info about the set of parameters.
+pub mod presign;
 pub mod sign;
 #[cfg(test)]
 mod test;
+
+use serde::{Deserialize, Serialize};
+use zeroize::ZeroizeOnDrop;
+use frost_core::round1::SigningNonces;
+use frost_core::round1::SigningCommitments;
 
 use crate::crypto::ciphersuite::{BytesOrder, Ciphersuite, ScalarSerializationFormat};
 
@@ -14,16 +22,28 @@ impl ScalarSerializationFormat for JubjubBlake2b512 {
 }
 impl Ciphersuite for JubjubBlake2b512 {}
 
-
+pub type SigningShare = crate::SigningShare<JubjubBlake2b512>;
 pub type KeygenOutput = crate::KeygenOutput<JubjubBlake2b512>;
+
+/// The necessary inputs for the creation of a presignature.
+pub struct PresignArguments {
+    /// The output of key generation, i.e. our share of the secret key, and the public key package.
+    pub keygen_out: KeygenOutput,
+}
+
+/// The output of the presigning protocol.
+///
+/// This output is basically all the parts of the signature that we can perform
+/// without knowing the message.
+#[derive(Debug, Clone, Serialize, Deserialize, Eq, PartialEq, ZeroizeOnDrop)]
+pub struct PresignOutput {
+    /// The public nonce commitment.
+    pub nonces: SigningNonces<JubjubBlake2b512>,
+    #[zeroize[skip]]
+    pub commitments: SigningCommitments<JubjubBlake2b512>,
+}
+
+
 /// Signature would be Some for coordinator and None for other participants
 pub type SignatureOption = Option<frost_core::Signature<JubjubBlake2b512>>;
 
-/// Abstracts over different RedJubJub parameter choices, [`Binding`]
-/// and [`SpendAuth`].
-pub use reddsa::SpendAuth;
-pub use reddsa::Binding;
-
-
-/// Randomizers for key pairs
-use reddsa::SigningKey;
--- src/eddsa/redjubjub/sign.rs
+++ src/eddsa/redjubjub/sign.rs
@@ -1,5 +1,6 @@
-use reddsa::SigType;
-
+//! This module and the frost one are supposed to have the same helper function
+//! However, currently the reddsa is implemented  wraps a signature generation functionality from `Frost` library
+//!  into `cait-sith::Protocol` representation.
 use super::{KeygenOutput, SignatureOption};
 use crate::errors::{InitializationError, ProtocolError};
 use crate::participants::{Participant, ParticipantList};
@@ -12,6 +13,8 @@ use frost_ed25519::{aggregate, rand_core, round1, round2, VerifyingKey};
 use rand_core::CryptoRngCore;
 use std::collections::BTreeMap;
 use zeroize::Zeroizing;
+
+use reddsa::SigType;
 
 use reddsa::SigningKey;
 
@@ -31,8 +34,265 @@ async fn do_sign_coordinator(
     me: Participant,
     keygen_output: KeygenOutput,
     message: Vec<u8>,
+    signature_randomizer: Randomizer<C>,
     rng: &mut impl CryptoRngCore,
 ) -> Result<SignatureOption, ProtocolError> {
+    // --- Round 1.
+    // * Wait for other parties' commitments.
 
-    unimplemented!()
+    let mut commitments_map: BTreeMap<frost_ed25519::Identifier, round1::SigningCommitments> =
+        BTreeMap::new();
+
+    let signing_share = SigningShare::new(keygen_output.private_share.to_scalar());
+
+    // Step 1.1 (and implicitely 1.2)
+    let (nonces, commitments) = round1::commit(&signing_share, rng);
+    let nonces = Zeroizing::new(nonces);
+    commitments_map.insert(me.to_identifier()?, commitments);
+
+    // Step 1.3
+    let commit_waitpoint = chan.next_waitpoint();
+
+    // Step 1.4
+    for (from, commitment) in recv_from_others(&chan, commit_waitpoint, &participants, me).await? {
+        commitments_map.insert(from.to_identifier()?, commitment);
+    }
+
+    let signing_package = frost_ed25519::SigningPackage::new(commitments_map, message.as_slice());
+
+    let mut signature_shares: BTreeMap<frost_ed25519::Identifier, round2::SignatureShare> =
+        BTreeMap::new();
+
+    // Step 1.5
+    let r2_wait_point = chan.next_waitpoint();
+    chan.send_many(r2_wait_point, &signing_package)?;
+
+    // --- Round 2
+    // * Wait for each other's signature share
+    // Step 2.3 (2.1 and 2.2 are implicit)
+    let vk_package = keygen_output.public_key;
+    let key_package = construct_key_package(threshold, me, signing_share, &vk_package)?;
+    let key_package = Zeroizing::new(key_package);
+    let signature_share = round2::sign(&signing_package, &nonces, &key_package)
+        .map_err(|e| ProtocolError::AssertionFailed(e.to_string()))?;
+
+    // Step 2.5 (2.4 is implicit)
+    signature_shares.insert(me.to_identifier()?, signature_share);
+    for (from, signature_share) in recv_from_others(&chan, r2_wait_point, &participants, me).await?
+    {
+        signature_shares.insert(from.to_identifier()?, signature_share);
+    }
+
+    // --- Signature aggregation.
+    // * Converted collected signature shares into the signature.
+    // * Signature is verified internally during `aggregate()` call.
+
+    // Step 2.6 and 2.7
+    // We supply empty map as `verifying_shares` because we have disabled "cheater-detection" feature flag.
+    // Feature "cheater-detection" only points to a malicious participant, if there's such.
+    // It doesn't bring any additional guarantees.
+    let public_key_package = PublicKeyPackage::new(BTreeMap::new(), vk_package);
+    let signature = aggregate(&signing_package, &signature_shares, &public_key_package)
+        .map_err(|e| ProtocolError::AssertionFailed(e.to_string()))?;
+
+    Ok(Some(signature))
+}
+
+
+/// A function that takes a signing share and a keygenOutput
+/// and construct a public key package used for frost signing
+fn construct_key_package(
+    threshold: usize,
+    me: Participant,
+    signing_share: SigningShare,
+    verifying_key: &VerifyingKey,
+) -> Result<KeyPackage, ProtocolError> {
+    let identifier = me.to_identifier()?;
+    let verifying_share = signing_share.into();
+
+    Ok(KeyPackage::new(
+        identifier,
+        signing_share,
+        verifying_share,
+        *verifying_key,
+        u16::try_from(threshold).map_err(|_| {
+            ProtocolError::Other("threshold cannot be converted to u16".to_string())
+        })?,
+    ))
+}
+
+
+
+/// Returns a future that executes signature protocol for *a Participant*.
+///
+/// WARNING: Extracted from FROST documentation:
+/// In all of the main FROST ciphersuites, the entire message must be sent
+/// to participants. In some cases, where the message is too big, it may be
+/// necessary to send a hash of the message instead. We strongly suggest
+/// creating a specific ciphersuite for this, and not just sending the hash
+/// as if it were the message.
+/// For reference, see how RFC 8032 handles "pre-hashing".
+async fn do_sign_participant(
+    mut chan: SharedChannel,
+    threshold: usize,
+    me: Participant,
+    coordinator: Participant,
+    keygen_output: KeygenOutput,
+    message: Vec<u8>,
+    rng: &mut impl CryptoRngCore,
+) -> Result<SignatureOption, ProtocolError> {
+    // --- Round 1.
+    if coordinator == me {
+        return Err(ProtocolError::AssertionFailed(
+            "the do_sign_participant function cannot be called
+            for a coordinator"
+                .to_string(),
+        ));
+    }
+
+    // create signing share out of private_share
+    let signing_share = SigningShare::new(keygen_output.private_share.to_scalar());
+
+    // Step 1.1
+    let (nonces, commitments) = round1::commit(&signing_share, rng);
+    // Ensures the values are zeroized on drop
+    let nonces = Zeroizing::new(nonces);
+
+    // * Wait for an initial message from a coordinator.
+    // * Send coordinator our commitment.
+
+    // Step 1.2
+    let commit_waitpoint = chan.next_waitpoint();
+    chan.send_private(commit_waitpoint, coordinator, &commitments)?;
+
+    // --- Round 2.
+    // * Wait for a signing package.
+    // * Send our signature share.
+
+    // Step 2.1
+    let r2_wait_point = chan.next_waitpoint();
+    let signing_package = loop {
+        let (from, signing_package): (_, frost_ed25519::SigningPackage) =
+            chan.recv(r2_wait_point).await?;
+        if from != coordinator {
+            continue;
+        }
+        break signing_package;
+    };
+
+    // Step 2.2
+    if signing_package.message() != message.as_slice() {
+        return Err(ProtocolError::AssertionFailed(
+            "Expected message doesn't match with the actual message received in a signing package"
+                .to_string(),
+        ));
+    }
+
+    // Step 2.3
+    let vk_package = keygen_output.public_key;
+    let key_package = construct_key_package(threshold, me, signing_share, &vk_package)?;
+    // Ensures the values are zeroized on drop
+    let key_package = Zeroizing::new(key_package);
+    let signature_share = round2::sign(&signing_package, &nonces, &key_package)
+        .map_err(|e| ProtocolError::AssertionFailed(e.to_string()))?;
+
+    // Step 2.4
+    chan.send_private(r2_wait_point, coordinator, &signature_share)?;
+
+    Ok(None)
+}
+
+/// Depending on whether the current participant is a coordinator or not,
+/// runs the signature protocol as either a participant or a coordinator.
+///
+/// WARNING: Extracted from FROST documentation:
+/// In all of the main FROST ciphersuites, the entire message must be sent
+/// to participants. In some cases, where the message is too big, it may be
+/// necessary to send a hash of the message instead. We strongly suggest
+/// creating a specific ciphersuite for this, and not just sending the hash
+/// as if it were the message.
+/// For reference, see how RFC 8032 handles "pre-hashing".
+pub fn sign(
+    participants: &[Participant],
+    threshold: usize,
+    me: Participant,
+    coordinator: Participant,
+    keygen_output: KeygenOutput,
+    message: Vec<u8>,
+    rng: impl CryptoRngCore + Send + 'static,
+) -> Result<impl Protocol<Output = SignatureOption>, InitializationError> {
+    if participants.len() < 2 {
+        return Err(InitializationError::NotEnoughParticipants {
+            participants: participants.len(),
+        });
+    }
+    let Some(participants) = ParticipantList::new(participants) else {
+        return Err(InitializationError::DuplicateParticipants);
+    };
+
+    // ensure my presence in the participant list
+    if !participants.contains(me) {
+        return Err(InitializationError::MissingParticipant {
+            role: "self",
+            participant: me,
+        });
+    }
+
+    // ensure the coordinator is a participant
+    if !participants.contains(coordinator) {
+        return Err(InitializationError::MissingParticipant {
+            role: "coordinator",
+            participant: coordinator,
+        });
+    }
+
+    let comms = Comms::new();
+    let chan = comms.shared_channel();
+    let fut = fut_wrapper(
+        chan,
+        participants,
+        threshold,
+        me,
+        coordinator,
+        keygen_output,
+        message,
+        rng,
+    );
+    Ok(make_protocol(comms, fut))
+}
+
+#[allow(clippy::too_many_arguments)]
+async fn fut_wrapper(
+    chan: SharedChannel,
+    participants: ParticipantList,
+    threshold: usize,
+    me: Participant,
+    coordinator: Participant,
+    keygen_output: KeygenOutput,
+    message: Vec<u8>,
+    mut rng: impl CryptoRngCore,
+) -> Result<SignatureOption, ProtocolError> {
+    if me == coordinator {
+        do_sign_coordinator(
+            chan,
+            participants,
+            threshold,
+            me,
+            keygen_output,
+            message,
+            &mut rng,
+        )
+        .await
+    } else {
+        do_sign_participant(
+            chan,
+            threshold,
+            me,
+            coordinator,
+            keygen_output,
+            message,
+            &mut rng,
+        )
+        .await
+    }
 }
